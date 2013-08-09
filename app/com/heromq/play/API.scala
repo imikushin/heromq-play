093ade298e26d6982cce30c5100312806e9b9a46
Ivan Mikushin <i.mikushin@gmail.com>
2013-08-09 15:53:16 +0600

    finish pubsub

--- app/com/heromq/play/API.scala
+++ app/com/heromq/play/API.scala
@@ -1,36 +1,31 @@
 package com.heromq.play
 
-import play.api.mvc._
-import play.api.libs.iteratee._
+import grizzled.slf4j.Logging
 import java.nio.charset.Charset
+import play.api.libs.iteratee._
+import play.api.mvc._
 import scala.language.reflectiveCalls
 
-case class Chunk(topic: String, chunk: String)
+case class Chunk(topic: String, payload: String)
 
-object API extends Controller {
+object API extends Controller with Logging {
 
   private val utf8 = Charset.forName("UTF-8")
+  private def str(bytes: Array[Byte]) = new String(bytes, utf8)
 
   val (enumerator, channel) = Concurrent.broadcast[Chunk]
 
   def pub(topic: String) = EssentialAction {rh =>
-    Iteratee.foreach {bytes: Array[Byte] => channel.push(Chunk(topic, new String(bytes, utf8)))} map {_ => Ok}
+    Iteratee.foreach[Array[Byte]] {bytes => channel.push(Chunk(topic, str(bytes)))} map {_ => Ok}
   }
 
   def filter(topic: String) = Enumeratee.filter[Chunk] {_.topic == topic}
 
-  def payload = Enumeratee.map[Chunk] {_.chunk}
-
-  def connDeathWatch[T](addr: String): Enumeratee[T, T] =
-    Enumeratee.onIterateeDone {() => println(addr + " - SSE disconnected")}
+  def payload = Enumeratee.map[Chunk] {_.payload}
 
   def sub(topic: String) = Action {req =>
-    println(req.remoteAddress + " - SSE connected")
-    Ok.stream(enumerator
-        &> filter(topic) &> payload
-        &> Concurrent.buffer(50)
-        &> connDeathWatch(req.remoteAddress)
-    ).as("text/plain")
+    debug(req.remoteAddress + " - client connected")
+    Ok.stream(enumerator &> filter(topic) &> payload &> Concurrent.buffer(50))
   }
 
 }
--- build.sbt
+++ build.sbt
@@ -1,5 +1,6 @@
 scalaVersion := "2.10.2"
 
 libraryDependencies ++= Seq(
+  "org.clapper" %% "grizzled-slf4j" % "1.0.1",
   "org.scala-lang" % "scalap" % "2.10.2"
 )
