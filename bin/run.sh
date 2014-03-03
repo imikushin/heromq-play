97bcea79c5839d2c2ddd35585e6059e2ac1c5aea
Ivan Mikushin <i.mikushin@gmail.com>
2014-03-03 20:52:13 +0600

    rm unneeded stuff from run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -3,10 +3,4 @@
 set -u
 set -e
 
-if [ ! -d .ssh ]; then
-  echo "Downloading ssh.tgz, unpacking to .ssh/ ..."
-  curl --silent --location ${KBILLING_SSH_TGZ:-"https://s3.amazonaws.com/kbilling/staging-QjP6jiri/ssh.tgz"} | tar xz
-  echo "... done."
-fi
-
 exec java -javaagent:newrelic/newrelic.jar $JAVA_OPTS -Dhttp.port=${PORT} -cp "target/universal/stage/lib/*" play.core.server.NettyServer
